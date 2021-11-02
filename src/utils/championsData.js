3c1e9de4c8abe81ac9a482fff7a5c2ea78881572
Frank Garcia <00frankgarcia@gmail.com>
2021-11-02 16:51:20 -0300

    object type definitions

--- src/utils/championsData.js
+++ src/utils/championsData.js
@@ -1,17 +1,30 @@
-import { Constants, replaceVersion } from '../services/riot'
+/**
+ * @typedef {Object} ChampionMinData
+ * @property {String} title - name of the champion
+ * @property {String} description - little description of the champion
+ * @property {String} image - file name of the champion's image
+ */
 
-function getChampionImageURL(gameVersion, championImageFile) {
+import { Constants, replaceVersion } from '../services/riot'
+import { Champion } from '../services/riot'; // eslint-disable-line no-unused-vars
+
+export function getChampionImageURL(champion) {
+  let gameVersion = champion.version
+  let championImageFile = champion.image?.full
   return replaceVersion(Constants.CHAMPION_IMG_URL, gameVersion) + championImageFile
 }
 
+/**
+ * @param {Champion[]} champions 
+ * @returns {ChampionMinData[]}
+ */
 function retrieveListData(champions) {
   let data = []
   champions.forEach(c => {
-    getChampionImageURL(c.version, c.image.full)
     data.push({
       title: c.name,
       description: c.title.charAt(0).toUpperCase() + c.title.slice(1),
-      image: getChampionImageURL(c.version, c.image.full)
+      image: getChampionImageURL(c)
     })
   });
   return data
