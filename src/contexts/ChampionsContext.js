39b95ad568bea8b2f2108873b79986bd39cfe3ba
Frank Garcia <00frankgarcia@gmail.com>
2021-11-04 12:57:41 -0300

    adding portal behaviour to the context

--- src/contexts/ChampionsContext.js
+++ src/contexts/ChampionsContext.js
@@ -1,30 +1,43 @@
-import { createContext } from "react";
-import { useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useReducer } from 'react';
+import { TransitionablePortal } from 'semantic-ui-react';
+import ChampionInfo from '../components/ChampionInfo';
 import { getChampions } from '../services/riot';
+import portalReducer from '../reducers/portalReducer';
 
 const ChampionsContext = createContext();
 
-const initialState = []
+const initialChampionsState = []
+const initialPortalState = { isOpen: false, champion: null }
+
 function ChampionsProvider({ children }) {
-  const [champions, setChampions] = useState(initialState)
+  const [champions, setChampions] = useState(initialChampionsState)
+  const [portal, portalDispatch] = useReducer(portalReducer, initialPortalState)
 
   useEffect(() => {
     async function fillData() {
       setTimeout(async () => {
         let champions = await getChampions();
+        // console.log('champions:', champions);
         setChampions(champions);
-        console.log(champions);
-      }, 20);
+      }, 200);
     }
     fillData();
   }, [])
 
   return (
-    <ChampionsContext.Provider value={{champions}}>
+    <ChampionsContext.Provider value={{ champions, portalDispatch }}>
       {children}
+      <TransitionablePortal
+        transition={{animation: "fly left", duration: 900}}
+        open={portal.isOpen}>
+        <div id="champion-portal">
+          <ChampionInfo champion={portal.champion} />
+        </div>
+      </TransitionablePortal>
     </ChampionsContext.Provider>
   )
 }
 
 export { ChampionsProvider }
 export default ChampionsContext
+
